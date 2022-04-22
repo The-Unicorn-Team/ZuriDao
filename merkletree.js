49788502860d2bea25f186e8975cbf000d483fac
Samuel Okpe <38431147+samokpe2@users.noreply.github.com>
2022-04-22 14:27:18 +0100

    updating merkle tree

--- merkletree.js
+++ merkletree.js
@@ -6,6 +6,7 @@ console.log("the merkleTress is loading.................................")
 let stakeholders = [
     "0x9F6Dd51f7a18Ce5D6FaFF9e5d3e5764Cca61cC44","0x20497F37a8169c8C9fA09411F8c2CFB7c90dE5d1","0x70FADB1887f906dF7060330b61ed16434d82189f","0x3849DDF392848582b860982740615b43AA537aC2","0x5d16FA7F1f7513e4603103dc353A284aA96BA7f4"
 ]
+const [ owner, secondAccount, thirdAccount] = await ethers.getSigners();
 
 // this creates a new array "leafnodes" by hashing the index of all stakeholders addresses using keccak256
 //then we create a merkletree object 
--- test/test.js
+++ test/test.js
@@ -1,19 +1,29 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const { MerkleTree } = require('merkletreejs')
+const keccak256 = require('keccak256')
+
+describe("Create Merkle Root", function () {
+  it("Should return the new greeting once it's changed", async function () {
+    const ZuriElection = await ethers.getContractFactory("ZuriElection");
+
+    console.log("deploying ZuriElection contract>>>>>>>>>>>>>")
+      const proxy = await upgrades.deployProxy(ZuriElection, []);
+      await proxy.deployed();
+
+      console.log("ZuriElection Contract Address is...\n", proxy.address);
+  });
+});
+
 
 describe("Greeter", function () {
   it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+    const ZuriElection = await ethers.getContractFactory("ZuriElection");
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    console.log("deploying ZuriElection contract>>>>>>>>>>>>>")
+      const proxy = await upgrades.deployProxy(ZuriElection, []);
+      await proxy.deployed();
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+      console.log("ZuriElection Contract Address is...\n", proxy.address);
   });
 });
