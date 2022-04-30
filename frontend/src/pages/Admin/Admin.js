bda51c08efb920e6f63a1d6466fa521058c870fb
IDOKO EMMANUEL <pidox31102002@gmail.com>
2022-04-30 13:10:42 +0100

    redefined candidate card UI

--- frontend/src/pages/Admin/Admin.js
+++ frontend/src/pages/Admin/Admin.js
@@ -14,12 +14,10 @@ import { contractAddress } from '../constants/constant';
 import abi from '../constants/abi.json';
 import { getProviderInfoFromChecksArray } from 'web3modal';
 
-
 const style = {
   Electbtn: `px-3 py-2  rounded hover:ml-6`,
 };
 const Admin = () => {
-  
   const classes = useStyles();
   const globalStyles = useGlobalStyles();
   const {
@@ -60,102 +58,112 @@ const Admin = () => {
   };
   const { ethereum } = window;
 
-const Election = () => {
-  const classes = useStyles();
-  const globalStyles = useGlobalStyles();
-  const { currentAccount, connectWallet, isStudent, getProof, candidateCount, vote, startElection, endElection } = useContext(AppContext);
-  const [candidateId, setCandidateId] = useState(0)
-  const [show, setShow] = useState(false);
-  const [contenders, setContenders] = useState([]);
-  const [contract, setContract] = useState("");
-  const [candidatesBool, showCandidates] = useState(false);
+  const Election = () => {
+    const classes = useStyles();
+    const globalStyles = useGlobalStyles();
+    const {
+      currentAccount,
+      connectWallet,
+      isStudent,
+      getProof,
+      candidateCount,
+      vote,
+      startElection,
+      endElection,
+    } = useContext(AppContext);
+    const [candidateId, setCandidateId] = useState(0);
+    const [show, setShow] = useState(false);
+    const [contenders, setContenders] = useState([]);
+    const [contract, setContract] = useState('');
+    const [candidatesBool, showCandidates] = useState(false);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
-  // fetch contract
-  const createEthereumContract = () => {
-    const provider = new ethers.providers.Web3Provider(ethereum);
-    const signer = provider.getSigner();
-    const ZuriContract = new ethers.Contract(contractAddress, abi.abi, signer);
+    // fetch contract
+    const createEthereumContract = () => {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const ZuriContract = new ethers.Contract(
+        contractAddress,
+        abi.abi,
+        signer,
+      );
 
-    return ZuriContract;
+      return ZuriContract;
+    };
+
+    // useEffect(() => {
+    //   createEthereumContract();
+    // }, [])
+
+    const handleVote = () => {
+      confirmAlert({
+        title: 'Confirm to submit',
+        message: 'Are you sure, You want to vote for this person',
+        buttons: [
+          {
+            label: 'Yes',
+            onClick: () => alert('Click Yes'),
+          },
+          {
+            label: 'No',
+            onClick: () => alert('Click No'),
+          },
+        ],
+      });
+    };
+
+    //  get all candidates
+    const getAllCandidates = async () => {
+      const contract = createEthereumContract();
+      console.log(contract);
+      try {
+        return await contract.getCandidates();
+      } catch (error) {
+        alert(error);
+      }
+    };
+
+    const fetch = async () => {
+      // handleShow();
+      let candidateArray = [];
+
+      const result = await getAllCandidates();
+      result.map((item) => {
+        candidateArray.push({
+          id: Number(ethers.utils.hexValue(item.id._hex).slice(2)),
+          name: item.name,
+          votes: Number(ethers.utils.hexValue(item.voteCount._hex).slice(2)),
+        });
+        setContenders(candidateArray);
+        // console.log(contenders)
+      });
+    };
+
+    const start = async () => {
+      await startElection();
+    };
+
+    const voteCandidate = async (id) => {
+      const contract = createEthereumContract();
+
+      try {
+        let root = [];
+        const merkleRoot = await contract.root();
+        root.push(ethers.utils.keccak256(merkleRoot));
+        console.log(root);
+        console.log(contract.vote(id, root));
+        // console.log(await contract.root())
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    useEffect(() => {
+      fetch();
+    }, [showCandidates]);
   };
-
-  // useEffect(() => {
-  //   createEthereumContract();
-  // }, [])
-
-  const handleVote = () => {
-    confirmAlert({
-      title: 'Confirm to submit',
-      message: 'Are you sure, You want to vote for this person',
-      buttons: [
-        {
-          label: 'Yes',
-          onClick: () => alert('Click Yes')
-        },
-        {
-          label: 'No',
-          onClick: () => alert('Click No')
-        }
-      ]
-    });
-  }
-
-  //  get all candidates
-  const getAllCandidates = async () => {
-    const contract = createEthereumContract()
-console.log(contract)
-    try {
-      return await contract.getCandidates()
-
-    }
-
-    catch (error) {
-      alert(error)
-
-    }
-  }
-
-  const fetch = async () => {
-    // handleShow();
-    let candidateArray = []
-
-    const result = await getAllCandidates()
-    result.map((item) => {
-      candidateArray.push({ id: Number(ethers.utils.hexValue(item.id._hex).slice(2)), name: item.name, votes: Number(ethers.utils.hexValue(item.voteCount._hex).slice(2)), })
-      setContenders(candidateArray)
-      // console.log(contenders)
-    })
-
-  }
-
-  const start = async () => {
-    await startElection()
-
-  }
-
-  const voteCandidate = async (id) => {
-    const contract = createEthereumContract()
-
-    try{
-      let root = []
-      const merkleRoot = await contract.root()
-      root.push(ethers.utils.keccak256(merkleRoot))
-      console.log(root)
-      console.log(contract.vote(id, root))
-      // console.log(await contract.root())
-    } catch(error){
-      console.log(error)
-    }
-  }
-
-
-  useEffect(() => {
-    fetch()
-  }, [showCandidates])
-}
 
   // start Election function
   const startElectionFunc = async () => {
@@ -201,12 +209,6 @@ console.log(contract)
             // onClick={makeResultsPrivateFunc}
           >
             Make Private
-          </button>
-          <button
-            className={`hover:bg-blue-500 ${style.Electbtn} hover:bg-opacity-25`}
-            // onClick={() => showCandidates(true)}
-          >
-            Get Candidate
           </button>
         </div>
       </div>
